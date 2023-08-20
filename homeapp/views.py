1c65dd915530555133a5cce00ae0520d6c65fcf8
mubashiralli <mubashirm.a.n@hotmail.com>
2023-08-20 07:59:12 +0500

    site complete 61%

--- homeapp/views.py
+++ homeapp/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import JsonResponse
-
+from .forms import MessageForm
 class HomeView(View):
     def get(self,request):
         return render(request,'base.html')
     def post(self,request):
         data = request.POST
-        return JsonResponse({'working':data,})
+        form = MessageForm(data)
+        if form.is_valid():
+            form.save()
+            response = {'status':200}
+        else:
+            response = {'error':form.errors}
+        return JsonResponse(response)
 # Create your views here.
